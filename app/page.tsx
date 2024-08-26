3d9d1d650598a95f7e239eb27f06722724ff79c8
solvman <solvman@gmail.com>
2024-08-26 07:49:39 -0500

    refactor: Improve credit card form validation and formatting

--- app/page.tsx
+++ app/page.tsx
@@ -52,14 +52,20 @@ export default function Home() {
 
   return (
     <main className="lg:flex lg:h-screen lg:min-h-[547px] lg:flex-row lg:gap-32">
+      <h1 className="sr-only">
+        Interactive Card Detail - Frontend mentor project submission
+      </h1>
       <section className="mb-20 h-[240px] bg-mobile bg-cover bg-no-repeat pt-8 lg:mb-0 lg:h-full lg:w-1/2 lg:bg-desktop lg:bg-contain lg:pt-0">
+        <h2 className="sr-only">Card detail</h2>
         <div className="wrapper lg:flex lg:h-full lg:flex-col-reverse lg:items-end lg:justify-center lg:gap-9 lg:px-0">
-          <div className="card relative ml-auto bg-card-back">
+          <article className="card relative ml-auto bg-card-back">
+            <h3 className="sr-only">Card back</h3>
             <p className="lg absolute right-10 top-1/2 -translate-y-1/2 text-[0.5625rem] text-inverted lg:right-14 lg:text-[0.875rem] lg:tracking-[2px]">
               {cardData.cvv}
             </p>
-          </div>
-          <div className="card relative -top-16 bg-card-front p-5 text-inverted shadow-2xl lg:top-0 lg:mr-24 lg:p-8">
+          </article>
+          <article className="card relative -top-16 bg-card-front p-5 text-inverted shadow-2xl lg:top-0 lg:mr-24 lg:p-8">
+            <h3 className="sr-only">Card front</h3>
             <Image
               height={30}
               width={54}
@@ -76,13 +82,15 @@ export default function Home() {
                 {cardData.expMonth.padStart(2, "0")}/{cardData.expYear}
               </p>
             </div>
-          </div>
+          </article>
         </div>
       </section>
       <section className="relative pb-11 pt-1 lg:h-full lg:w-1/2 lg:p-0">
+        <h2 className="sr-only">Card detail submission</h2>
         <div className="wrapper lg:mx-0 lg:flex lg:h-full lg:max-w-[381px] lg:flex-col lg:items-start lg:justify-center lg:px-0">
           {isSubmitted ? (
-            <div className="flex w-full flex-col items-center justify-center">
+            <article className="flex w-full flex-col items-center justify-center">
+              <h3 className="sr-only">Card detail has been submitted</h3>
               <Image
                 src={imageComplete}
                 alt=""
@@ -91,126 +99,132 @@ export default function Home() {
                 height={80}
                 className="mb-9"
               />
-              <h3 className="mb-4 text-[28px] font-medium uppercase tracking-[3.422px] text-primary">
+              <p className="mb-4 text-[28px] font-medium uppercase tracking-[3.422px] text-primary">
                 Thank you!
-              </h3>
+              </p>
               <p className="mb-12 text-lg text-secondary-200">
                 We&apos;ve added your credit card detail
               </p>
               <button onClick={onContinue} className="btn-primary w-full">
                 Continue
               </button>
-            </div>
+            </article>
           ) : (
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="flex flex-col gap-5 lg:gap-[26px]">
-                <div>
-                  <label htmlFor="name" className="label">
-                    <span className="block">Cardholder name</span>{" "}
-                  </label>
-                  <TextInput
-                    type="text"
-                    id="name"
-                    placeholder="e.g. Jane Appleseed"
-                    {...register("name", { required: ERROR_REQUIRED })}
-                    error={errors.name?.message}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="card-number" className="label">
-                    <span className="block">Card number</span>
-                  </label>
-                  <Controller
-                    control={control}
-                    name="cardNumber"
-                    rules={{
-                      required: ERROR_REQUIRED,
-                      minLength: { value: 19, message: "Must be valid number" },
-                    }}
-                    render={({ field: { onChange, value, ref } }) => {
-                      return (
-                        <TextInput
-                          ref={ref}
-                          type="text"
-                          id="card-number"
-                          value={value}
-                          onChange={(event) =>
-                            onChange(formatToCardNumber(event.target.value))
-                          }
-                          placeholder="e.g. 1234 5678 9876 5432"
-                          error={errors.cardNumber?.message}
-                        />
-                      );
-                    }}
-                  />
-                </div>
-                <div className="flex flex-row items-start gap-3">
-                  <div className="flex w-1/2 flex-col">
-                    <label htmlFor="exp-month" className="label basis-full">
-                      <span>Exp. date (MM/YY)</span>
+            <article>
+              <h2 className="sr-only">Card detail submission form</h2>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-5 lg:gap-[26px]">
+                  <div>
+                    <label htmlFor="name" className="label">
+                      <span className="block">Cardholder name</span>{" "}
                     </label>
-                    <div className="flex flex-row gap-1">
-                      <TextInput
-                        type="number"
-                        id="exp-month"
-                        placeholder="MM"
-                        {...register("expMonth", {
-                          required: ERROR_REQUIRED,
-                          min: { value: 1, message: "Not valid" },
-                          max: { value: 12, message: "Not valid" },
-                        })}
-                        error={errors.expMonth?.message}
-                      />
-                      <TextInput
-                        type="number"
-                        id="exp-year"
-                        placeholder="YY"
-                        {...register("expYear", {
-                          required: ERROR_REQUIRED,
-                          min: { value: 24, message: "Not valid year" },
-                          max: { value: 99, message: "Not valid year" },
-                        })}
-                        error={errors.expYear?.message}
-                      />
-                    </div>
+                    <TextInput
+                      type="text"
+                      id="name"
+                      placeholder="e.g. Jane Appleseed"
+                      {...register("name", { required: ERROR_REQUIRED })}
+                      error={errors.name?.message}
+                    />
                   </div>
-                  <div className="flex w-1/2 flex-col">
-                    <label htmlFor="cvv" className="label">
-                      <span>CVV</span>
+                  <div>
+                    <label htmlFor="card-number" className="label">
+                      <span className="block">Card number</span>
                     </label>
                     <Controller
                       control={control}
-                      name="cvv"
+                      name="cardNumber"
                       rules={{
                         required: ERROR_REQUIRED,
                         minLength: {
-                          value: 3,
-                          message: "Must be valid CVV",
+                          value: 19,
+                          message: "Must be valid number",
                         },
                       }}
-                      render={({ field: { value, onChange, ref } }) => {
+                      render={({ field: { onChange, value, ref } }) => {
                         return (
                           <TextInput
                             ref={ref}
                             type="text"
-                            id="cvv"
+                            id="card-number"
                             value={value}
-                            onChange={(event) => {
-                              onChange(formatToCVV(event.target.value));
-                            }}
-                            placeholder="e.g. 123"
-                            error={errors.cvv?.message}
+                            onChange={(event) =>
+                              onChange(formatToCardNumber(event.target.value))
+                            }
+                            placeholder="e.g. 1234 5678 9876 5432"
+                            error={errors.cardNumber?.message}
                           />
                         );
                       }}
                     />
                   </div>
+                  <div className="flex flex-row items-start gap-3">
+                    <div className="flex w-1/2 flex-col">
+                      <label htmlFor="exp-month" className="label basis-full">
+                        <span>Exp. date (MM/YY)</span>
+                      </label>
+                      <div className="flex flex-row gap-1">
+                        <TextInput
+                          type="number"
+                          id="exp-month"
+                          placeholder="MM"
+                          {...register("expMonth", {
+                            required: ERROR_REQUIRED,
+                            min: { value: 1, message: "Not valid" },
+                            max: { value: 12, message: "Not valid" },
+                          })}
+                          error={errors.expMonth?.message}
+                        />
+                        <TextInput
+                          type="number"
+                          id="exp-year"
+                          placeholder="YY"
+                          {...register("expYear", {
+                            required: ERROR_REQUIRED,
+                            min: { value: 24, message: "Not valid year" },
+                            max: { value: 99, message: "Not valid year" },
+                          })}
+                          error={errors.expYear?.message}
+                        />
+                      </div>
+                    </div>
+                    <div className="flex w-1/2 flex-col">
+                      <label htmlFor="cvv" className="label">
+                        <span>CVV</span>
+                      </label>
+                      <Controller
+                        control={control}
+                        name="cvv"
+                        rules={{
+                          required: ERROR_REQUIRED,
+                          minLength: {
+                            value: 3,
+                            message: "Must be valid CVV",
+                          },
+                        }}
+                        render={({ field: { value, onChange, ref } }) => {
+                          return (
+                            <TextInput
+                              ref={ref}
+                              type="text"
+                              id="cvv"
+                              value={value}
+                              onChange={(event) => {
+                                onChange(formatToCVV(event.target.value));
+                              }}
+                              placeholder="e.g. 123"
+                              error={errors.cvv?.message}
+                            />
+                          );
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <button type="submit" className="btn-primary">
+                    Submit
+                  </button>
                 </div>
-                <button type="submit" className="btn-primary">
-                  Submit
-                </button>
-              </div>
-            </form>
+              </form>
+            </article>
           )}
         </div>
       </section>
